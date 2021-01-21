3ec3ae3af2548980460f78431f974c985cc65e1f
anita2498 <anitaputhenpurayil@gmail.com>
2021-01-21 10:24:35 -0500

    Project complete

---
+++ src/main/java/com/hcl/productdetails/DrinkDAO.java
@@ -0,0 +1,5 @@
+package com.hcl.productdetails;
+
+public interface DrinkDAO extends GenericDAO<DrinkDTO> {
+
+}
---
+++ src/main/webapp/com/hcl/productdetails/Servlet0.java
@@ -0,0 +1,63 @@
+package com.hcl.productdetails;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class Servlet0
+ */
+@WebServlet("/Servlet0")
+public class Servlet0 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet0() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		  response.setContentType("text/html");
+//
+//	        PrintWriter out = response.getWriter();
+//	        out.println("<form action='' method='POST'>");
+//	        out.println("<label>Enter Product (Drink) ID: <input type='text' name='drink-id'></input></label>");
+//	        out.println("<input type='submit'>Get Details</input>");
+//	        out.println("</form>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 String drinkId = request.getParameter("drink-id");
+	        PrintWriter out = response.getWriter();
+	        DrinkDAO d= new DrinkDAOImpl();
+	        DrinkDTO d1 =(d.productInfo((Integer.parseInt(drinkId))));
+	        if (d1!=null) {
+	        System.out.println("Product found :  "+d1.toString());
+	        out.println("Product found <br>");
+	        out.println("Product Id : "+ d1.getId()+"<br>");
+	        out.println("Product Name : "+ d1.getName()+"<br>");
+	        out.println("Product is "+ (d1.isGood()? "Good.":"Bad."));}
+	        else 
+	        	 out.println("Product not found");
+	        
+	}
+
+}
